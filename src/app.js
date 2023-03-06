3d54ddbf2e8ce31e9ebd49cecad0c49ed2ddae7d
Hikitodn <Huy225dn@gmail.com>
2023-03-06 22:54:50 +0700

    setup alias modules

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-import { Express } from "express";
+import express, { json } from "express";
+import * as dotenv from "dotenv";
+// import test from "./api/test";
+
+// setup
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+// use
+app.use(json());
+
+app.get("/", async (req, res) => {
+  res.json({ status: true, message: "Our node.js app works" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on: http://localhost:${PORT}`);
+});
